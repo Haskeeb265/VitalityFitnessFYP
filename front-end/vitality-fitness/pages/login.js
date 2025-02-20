cd8296e068bad3429056774b605205cfe42ff079
Haseebullah <haskeebullah265@gmail.com>
2025-02-20 16:02:12 +0500

    Backup before installing Shadcn

--- front-end/vitality-fitness/pages/login.js
+++ front-end/vitality-fitness/pages/login.js
@@ -1,8 +1,30 @@
-
+import Image from "next/image";
 
 export default function LoginPage() {
   
   return (
-    <h1>Login</h1>
-  );
+    <div className="login-container">
+
+
+        <div className="login-logo ">
+            <Image src="/img/login-logo.png" alt="Login Logo" width={120} height={120} />
+        </div>
+        
+        <div className="login-heading">
+            <h1 className="text-[48px] text-[#DB4A2B] ">WELCOME TO VITALITY FITNESS</h1>
+        </div>
+
+        <div className="login-subheading">
+            <h2 className="text-[32px] sf-bold">LOGIN</h2>
+        </div>
+
+        <div className="login-subtext">
+            <h2 className="text-[16px]">Please enter your email and password</h2>
+        </div>
+
+
+    </div>
+  
+
+);
 }
